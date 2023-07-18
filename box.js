b90512be39a2260842eb68b93fd90e5acb533d83
aartibiradar21 <95897636+aartibiradar21@users.noreply.github.com>
2023-07-18 11:27:47 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,17 @@
+var button=document.getElementById('box');
+var button1=document.getElementById('button1');
+var button2=document.getElementById('button2');
+var button3=document.getElementById('button3');
+
+
+
+button1.onclick=function(){
+     button.style.backgroundColor="Red"
+}
+button2.onclick=function(){
+     button.style.backgroundColor="Yellow"
+}
+button3.onclick=function(){
+     button.style.backgroundColor="Blue"
+}
+     
